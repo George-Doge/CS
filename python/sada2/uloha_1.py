b38d9d40304008af2eb9df78e9df9e8da3d19ec5
Juraj-J <juraj.jencar@gmail.com>
2024-01-18 20:44:28 +0100

    working coord

--- python/sada2/uloha_1.py
+++ python/sada2/uloha_1.py
@@ -1,4 +1,3 @@
-#ULOHA 1
 import tkinter, random
 
 window = tkinter.Tk()
@@ -7,39 +6,45 @@ canvas.pack()
 
 #DECLARE VARIABLES
 score_count = 0
+last_click = (0, 0)
 
-def square(x, y):
+def square():
+    x_sqr = random.randint(80, 720)
+    y_sqr = random.randint(80, 720)
     a = 80
-    canvas.create_rectangle(x-a, y-a, x+a, y+a, fill="green")
+    canvas.create_rectangle(x_sqr-a, y_sqr-a, x_sqr+a, y_sqr+a, fill="green")
+
+    #check if the square is clicked
+    x = last_click[0]
+    y = last_click[1]
+
+    print(x_sqr-a, x_sqr+a)
+
+    # if x_sqr-a>x>x_sqr+a:
+    #     print("Je to v X")
 
 
 def score(score):
-        canvas.create_text(100, 50, text=f"Your score: {score}", font="Arial 20")
+    canvas.create_text(100, 50, text=f"Your score: {score}", font="Arial 20")
 
 
-def clicked(coord, x_sqr, y_sqr):
-     x = coord.x
-     y = coord.y
+def clicked(coord):
+    global last_click
+    cursor_coord = (coord.x, coord.y)
 
-     #dat tu moznost kde skontroluje, ci je inbounds stvorca
-     if x > x_sqr and x < x_sqr:
-        print("It is in X")
-
-        if y > y_sqr and y < y_sqr:
-            print("It is in Y")
-
+    last_click = cursor_coord
+    print(last_click)
+     
 
 while True:
-    x_sqr = random.randint(80, 720)
-    y_sqr = random.randint(80, 720)
 
-    square(x_sqr, y_sqr)
-
-    canvas.bind("<Button-1>",clicked(x_sqr, y_sqr))
+    canvas.bind("<Button-1>",clicked)
+    
+    square()
 
     score(score_count)
 
-    canvas.after(1000)
+    canvas.after(3000)
     canvas.update()
     canvas.delete("all")
 
