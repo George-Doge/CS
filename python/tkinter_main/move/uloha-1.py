769a50d44b91ea259e393c0f93ea8ed89c00b186
Juraj-J <juraj.jencar@gmail.com>
2024-01-22 19:20:09 +0100

    working tasks moving 1/2

--- python/tkinter_main/move/uloha-1.py
+++ python/tkinter_main/move/uloha-1.py
@@ -1,7 +1,7 @@
 import tkinter
 
 window = tkinter.Tk()
-canvas = tkinter.Canvas(width=800, height=800, bg="white")
+canvas = tkinter.Canvas(width=800, height=400, bg="white")
 canvas.pack()
 
 
@@ -14,15 +14,24 @@ def auto():
     canvas.create_oval(x-r+50,y-r+40, x+r+50, y+r+40, fill="yellow",tags="auto")
 
 
-def bicycle():
-    pass
+def scooter():
+    x, y = 500, 150
+    r = 30
+    a, b = 70, 10
+
+    canvas.create_rectangle(x-a, y-b, x+a, y+b, fill="black", tags="scooter")
+    canvas.create_oval(x-r-a, y-r, x+r-a, y+r,fill="black", tags="scooter")
+    canvas.create_oval(x-r+a, y-r, x+r+a, y+r,fill="black", tags="scooter")
+    canvas.create_rectangle(x-b-a, y-2*a, x+b-a, y, fill="black", tags="scooter")
 
 
 auto()
-bicycle()
+scooter()
+
 
 while True:
     canvas.move("auto", 10, 0)
+    canvas.move("scooter", -10, 0)
     canvas.after(20)
     canvas.update()
 
--- python/tkinter_main/move/uloha-2.py
+++ python/tkinter_main/move/uloha-2.py
@@ -1,31 +1,53 @@
 import tkinter
 
 window = tkinter.Tk()
-canvas = tkinter.Canvas(width=800, height=800, bg="white")
+canvas = tkinter.Canvas(width=800, height=400, bg="white")
 canvas.pack()
 
 
 def auto(x_car):
+    x = x_car
     y = 300
     a, b = 35, 70
     r = 20
-    canvas.create_rectangle(x_car-b, y-a, x_car+b, y+a, fill="blue", tags="auto")
-    canvas.create_oval(x_car-r-50,y-r+40, x_car+r-50, y+r+40, fill="yellow",tags="auto")
-    canvas.create_oval(x_car-r+50,y-r+40, x_car+r+50, y+r+40, fill="yellow",tags="auto")
+    canvas.create_rectangle(x-b, y-a, x+b, y+a, fill="blue", tags="auto")
+    canvas.create_oval(x-r-50,y-r+40, x+r-50, y+r+40, fill="yellow",tags="auto")
+    canvas.create_oval(x-r+50,y-r+40, x+r+50, y+r+40, fill="yellow",tags="auto")
+
+
+def scooter(x_scooter):
+    x = x_scooter
+    y = 200
+    r = 30
+    a, b = 70, 10
+
+    canvas.create_rectangle(x-a, y-b, x+a, y+b, fill="black", tags="scooter")
+    canvas.create_oval(x-r-a, y-r, x+r-a, y+r,fill="black", tags="scooter")
+    canvas.create_oval(x-r+a, y-r, x+r+a, y+r,fill="black", tags="scooter")
+    canvas.create_rectangle(x-b-a, y-2*a, x+b-a, y, fill="black", tags="scooter")
 
 
 #SETUP OF VARIABLES
-global x_car
-x_car = 100
+global x_car, x_scooter
+x_car, x_scooter = 100, 600
+scooter_speed, car_speed = 15, 10
 
 while True:
 
     auto(x_car)
     if x_car <= 850:
-        canvas.move("auto", 10, 0)
-        x_car += 10
+        canvas.move("auto", car_speed, 0)
+        x_car += car_speed
     else:
-        x_car = 0
+        x_car = -50
+
+    scooter(x_scooter)
+    if x_scooter >= -50:
+        canvas.move("scooter", -scooter_speed, 0)
+        x_scooter -= scooter_speed
+    else:
+        x_scooter = 950
+
 
     canvas.after(20)
     canvas.update()
