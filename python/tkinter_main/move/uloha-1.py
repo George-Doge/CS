f2881dba9ec0d741f4e0bd872d723b7ccb01167d
Juraj-J <juraj.jencar@gmail.com>
2024-01-22 16:24:01 +0100

    WIP task1

---
+++ python/tkinter_main/move/uloha-1.py
@@ -0,0 +1,20 @@
+import tkinter
+
+window = tkinter.Tk()
+canvas = tkinter.Canvas(width=800, height=800, bg="white")
+canvas.pack()
+
+
+def auto():
+    x , y = 500, 300
+    a, b = 35, 70
+    r = 20
+    canvas.create_rectangle(x-b, y-a, x+b, y+a, fill="blue", tags="auto")
+    canvas.create_oval(x-r-50,y-r+40, x+r-50, y+r+40, fill="yellow",tags="auto")
+    canvas.create_oval(x-r+50,y-r+40, x+r+50, y+r+40, fill="yellow",tags="auto")
+
+
+
+auto()
+
+window.mainloop()
---
+++ python/tkinter_main/move/uloha-2.py
@@ -0,0 +1,8 @@
+import tkinter
+
+window = tkinter.Tk()
+canvas = tkinter.Canvas(width=800, height=800, bg="white")
+canvas.pack()
+
+
+window.mainloop()
