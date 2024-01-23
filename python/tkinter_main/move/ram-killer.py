e18eba971a90c7ab1bfd267f4eadf03d0358f994
Juraj-J <juraj.jencar@gmail.com>
2024-01-23 16:24:59 +0100

    small ram killer

---
+++ python/tkinter_main/move/ram-killer.py
@@ -0,0 +1,39 @@
+import tkinter
+
+window = tkinter.Tk()
+canvas = tkinter.Canvas(width=800, height=200, bg="blue")
+canvas.pack()
+
+
+def cloud():
+    x, y = 75, 100
+    r = 40
+
+    canvas.create_oval(x-r-30, y-r, x+r+30, y+r, fill="white", tags="mrak")
+
+
+def sun():
+    x, y = 750, 100
+    r = 50
+    canvas.create_oval(x-r, y-r, x+r, y+r, fill="yellow")
+
+
+def move(coord):
+        x_counter = 75
+        for i in range(63):
+            canvas.move('mrak', 10, 0)
+            x_counter += 10
+            canvas.after(20)
+            canvas.update()
+
+        while x_counter >= 600:
+            cloud()
+
+
+sun()
+cloud()
+while True:
+    canvas.bind("<Button-1>", move)
+    canvas.update()
+
+window.mainloop()
--- python/tkinter_main/move/uloha-3.py
+++ python/tkinter_main/move/uloha-3.py
@@ -4,5 +4,9 @@ window = tkinter.Tk()
 canvas = tkinter.Canvas(width=800, height=800, bg="white")
 canvas.pack()
 
+#zaba1 = tkinter.PhotoImage(file='zaba1.png')
+zaba2 = tkinter.PhotoImage(file='zaba2.png')
+
+canvas.create_image(200, 100, image=zaba2)
 
 window.mainloop()
