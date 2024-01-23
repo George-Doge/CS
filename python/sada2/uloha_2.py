b1ce326c2125d58a74f949b0c96729e4e1db63dc
Juraj-J <juraj.jencar@gmail.com>
2024-01-23 22:30:52 +0100

    added falling and score losing mechanic

--- python/sada2/uloha_2.py
+++ python/sada2/uloha_2.py
@@ -27,7 +27,6 @@ class Bat:
         global x_bat
         x_bat = self.x
         x_bat -= 30
-        print(self.x)
 
 
     def move_right(self, coord):
@@ -37,27 +36,60 @@ class Bat:
         x_bat += 30
 
 
+class Ball:
+    def __init__(self, y_ball):
+        x = 400
+        self.x, self.y = x, y_ball
+        r = 15
+        self.center = (self.x, self.y)
+        self.coord = (self.center[0]-r, self.center[1]-r, self.center[0]+r, self.center[1]+r)
+
+
+    def draw(self):
+        canvas.create_oval(self.coord, fill="orange", tags="ball")
+
+
+    def fall(self):
+        global y_ball, score_count, score_per_sec
+        y_ball = self.y
+        if self.y <= 800:
+            y_ball += 90
+        else:
+            y_ball = 17
+        if self.y > 800 and not score_per_sec:
+            score_count -= 1
+            score_per_sec = True
+
+
+
 #define variables
 x_bat = 400
-
+y_ball = 17
+score_count = 0
+score_per_sec = False
 
 while True:
     frames += 1
 
     if frames > 60:
         frames = 1
-
+        score_per_sec = False
     if frames == 1:
+        lopta = Ball(y_ball)
         bat = Bat(x_bat)
 
     bat.draw()
+    lopta.draw()
+    lopta.fall()
 
     canvas.bind("<Button-1>", bat.move_left)
 
     canvas.bind("<Button-3>", bat.move_right)
 
-    print(bat.x)
-    canvas.after(5)
+    canvas.after(2)
+
+    canvas.create_text(100, 50, text=f"Your score: {score_count}", font="Arial 20")
+
     canvas.update()
     canvas.delete("all")
 
