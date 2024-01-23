71614effe16fca21003dbedd3a00f414c0bbf80e
Juraj-J <juraj.jencar@gmail.com>
2024-01-23 22:03:07 +0100

    movement working

--- python/sada2/uloha_2.py
+++ python/sada2/uloha_2.py
@@ -11,15 +11,34 @@ frames = 0
 
 class Bat:
 
-    def __init__(self):
-        a, b = 100, 10 
-        self.center = (400, 750)
+    def __init__(self, x_bat):
+        a, b = 100, 10
+        self.x = x_bat
+        self.center = (self.x, 750)
         self.coord = (self.center[0]-a, self.center[1]-b, self.center[0]+a, self.center[1]+b)
 
 
     def draw(self):
-        canvas.create_rectangle(self.coord, fill="blue")
+        canvas.create_rectangle(self.coord, fill="blue", tags="bat")
 
+
+    def move_left(self, coord):
+        # canvas.move("bat", -10, 0)
+        global x_bat
+        x_bat = self.x
+        x_bat -= 30
+        print(self.x)
+
+
+    def move_right(self, coord):
+        # canvas.move("bat", 10, 0)
+        global x_bat
+        x_bat = self.x
+        x_bat += 30
+
+
+#define variables
+x_bat = 400
 
 
 while True:
@@ -29,11 +48,16 @@ while True:
         frames = 1
 
     if frames == 1:
-        new_bat = Bat()
+        bat = Bat(x_bat)
 
-    new_bat.draw()
+    bat.draw()
 
+    canvas.bind("<Button-1>", bat.move_left)
 
+    canvas.bind("<Button-3>", bat.move_right)
+
+    print(bat.x)
+    canvas.after(5)
     canvas.update()
     canvas.delete("all")
 
