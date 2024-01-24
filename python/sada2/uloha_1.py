f39ebbe8b738d0019c688752340670f1fcd0b016
Juraj-J <juraj.jencar@gmail.com>
2024-01-24 12:33:38 +0100

    placem2

--- python/sada2/uloha_1.py
+++ python/sada2/uloha_1.py
@@ -5,62 +5,37 @@ canvas = tkinter.Canvas(width=800, height=800, bg="white")
 canvas.pack()
 
 #DECLARE VARIABLES
-score_count = 0
-last_click = (0, 0)
+score = 0
+cursor_coord = (0, 0)
 
 def square():
     #check if the square is clicked
-    canvas.delete("all")
-    global x_old
-    global y_old
-    x = last_click[0]
-    y = last_click[1]
+    canvas.delete("kocka")
+    # x = last_click[0]
+    # y = last_click[1]
     a = 80
-    x_old, y_old = 999, 999
-
-    # print(f"HEADER\nCLICKED:{last_click}\nSQUARE POS:{x_sqr-a, x_sqr+a}\nSTRED:{x_sqr}\nSCORE:{score_count}\n")
-
-    if x>=x_old-a and x<=x_old+a and y>=y_old-a and y<=y_old+a:
-        # score_count += 1
-        canvas.delete("kocka")
-    # else:
-    #     score_count -= 1
-    x_sqr = random.randint(80, 720)
-    y_sqr = random.randint(80, 720)
-    # x_sqr = 380
-    # y_sqr = 300
-
+    x_sqr = random.randint(50, 750)
+    y_sqr = random.randint(50, 750)
 
     canvas.create_rectangle(x_sqr-a, y_sqr-a, x_sqr+a, y_sqr+a, fill="green", tags="kocka")
 
-
-    x_old = x_sqr
-    y_old = y_sqr
+    print(cursor_coord)
 
     # canvas.delete("kocka")
     canvas.after(1000, square)
     canvas.update()
 
 
-
-def score(score):
-    canvas.create_text(100, 50, text=f"Your score: {score}", font="Arial 20", tags="zobrazovac")
-
-
 def clicked(coord):
-    global last_click
+    global cursor_coord
     cursor_coord = (coord.x, coord.y)
 
-    last_click = cursor_coord
-     
-
-
-score(score_count)
 
 canvas.bind("<Button-1>",clicked)
 
 square()
 
-canvas.delete("zobrazovac")
+canvas.create_text(100, 50, text=f"Your score: {score}", font="Arial 20", tags="zobrazovac")
+
 
 window.mainloop()
