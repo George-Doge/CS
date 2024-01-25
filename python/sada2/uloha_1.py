c38e993b644ff0d9a3f455076709fd129d780c28
Juraj-J <juraj.jencar@gmail.com>
2024-01-25 13:27:16 +0100

    sada2/uloha_1.py now working

--- python/sada2/uloha_1.py
+++ python/sada2/uloha_1.py
@@ -5,37 +5,41 @@ canvas = tkinter.Canvas(width=800, height=800, bg="white")
 canvas.pack()
 
 #DECLARE VARIABLES
+global score
 score = 0
-cursor_coord = (0, 0)
+
 
 def square():
     #check if the square is clicked
-    canvas.delete("kocka")
-    # x = last_click[0]
-    # y = last_click[1]
+    canvas.delete("all")
+    global score, x_sqr, y_sqr, a
     a = 80
     x_sqr = random.randint(50, 750)
     y_sqr = random.randint(50, 750)
 
     canvas.create_rectangle(x_sqr-a, y_sqr-a, x_sqr+a, y_sqr+a, fill="green", tags="kocka")
 
-    print(cursor_coord)
 
-    # canvas.delete("kocka")
-    canvas.after(1000, square)
-    canvas.update()
+    canvas.create_text(100, 50, text=f"Your score: {score}", font="Arial 20", tags="zobrazovac")
+
+
+    canvas.after(2000, square)
 
 
 def clicked(coord):
-    global cursor_coord
-    cursor_coord = (coord.x, coord.y)
+    global cursor_coord, score
+    # cursor_coord = (coord.x, coord.y)
+    x = coord.x 
+    y = coord.y
+    if x_sqr-a<= x <= x_sqr+a and y_sqr-a <= y <= y_sqr+a:
+        score += 1
+        canvas.delete("kocka")
+    else:
+        score -= 1
 
 
 canvas.bind("<Button-1>",clicked)
 
 square()
-
-canvas.create_text(100, 50, text=f"Your score: {score}", font="Arial 20", tags="zobrazovac")
-
 
 window.mainloop()
