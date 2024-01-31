baa50497de2a478d05f8f9f416dec1b022725d89
Juraj-J <juraj.jencar@gmail.com>
2024-01-31 12:36:55 +0100

    touples tastk 3,4

---
+++ python/touples/uloha-3.py
@@ -0,0 +1,6 @@
+cisla = (10, 155, 541, 4, 8544, 2, 0.5, 1, 525, 9)
+sucet = 0
+for num in cisla:
+    sucet += num
+
+print(sucet)
---
+++ python/touples/uloha-4.py
@@ -0,0 +1,6 @@
+cisla = (10, 9)
+sucin = 1
+for num in cisla:
+    sucin *= num
+
+print(sucin)
