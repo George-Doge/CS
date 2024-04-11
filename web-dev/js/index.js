8fa19932ab4ff976219f13ffc3a00e35aec2aaef
George-Doge <juraj.jencar@gmail.com>
2024-04-11 07:55:14 +0200

    2nd JS lesson

--- web-dev/js/index.js
+++ web-dev/js/index.js
@@ -1,5 +1,46 @@
-let userInput = prompt("Please enter your name: ");
+// TASK 1
+// let userInput = prompt("Please enter your name: ");
 
-if (userInput === "James" || userInput === "Mike"){
-    alert("Hi James or Mike!");
+// if (userInput === "James" || userInput === "Mike"){
+//     alert("Hi James or Mike!");
+// }
+
+// function greedPerson(){
+//     console.log("Hello " + userInput);
+// }
+
+// var sayGoodbye = function(){
+//     console.log("Bye");
+// }
+
+
+// if (userInput === "James") {
+//     alert("Hi James");
+// } 
+// else if (userInput === "Mike") {
+//     alert("Hi Mike!");
+// }
+
+// greedPerson();
+// sayGoodbye();
+
+
+// TASK 2
+function checkAge(){
+    let userAge = prompt("Please enter your age: ");
+
+    if (userAge > 18) {
+        console.log("The car is ready!");
+    }
+    else if (userAge == 18) {
+        console.log("First year driving. Enjoy your ride!");
+    }
+    else if (userAge => 1 & userAge < 18) {
+        console.log("You can't drive this car. Sorry.");
+    }
+    else if (userAge < 1) {
+        console.log("Interesting age.");
+    }
 }
+
+checkAge();
